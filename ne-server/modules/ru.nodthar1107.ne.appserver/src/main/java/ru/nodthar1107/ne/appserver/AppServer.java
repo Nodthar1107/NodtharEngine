ee463ce9acb115d3e1d30f9904e75da62ef5af8d
Nodthar1107 <vlad_shumilin@bk.ru>
2024-05-14 03:02:28 +0300

    Добавил возможность отправки сообщений на сервер

--- ne-server/modules/ru.nodthar1107.ne.appserver/src/main/java/ru/nodthar1107/ne/appserver/AppServer.java
+++ ne-server/modules/ru.nodthar1107.ne.appserver/src/main/java/ru/nodthar1107/ne/appserver/AppServer.java
@@ -1,20 +1,25 @@
 package ru.nodthar1107.ne.appserver;
 
+import org.glassfish.tyrus.server.Server;
+
 import com.google.inject.AbstractModule;
+
+import jakarta.websocket.DeploymentException;
 
 public class AppServer
 {
 	private static int PORT = 8000;
-	private static String HOST = "localhsot";
+	private static String HOST = "localhost";
 	
 	private static String PATH_ARG = "--path";
+	
 	private static String projectPath;
 	
 	private AbstractModule[] modules;
 	
-	public static void main(String[] args) throws Exception
+	public static void main(String[] args)
 	{
-		new AppServer().start(args);
+		new AppServer().launch(args);
 	}
 	
 	private AppServer(AbstractModule... modules)
@@ -22,7 +27,7 @@ public class AppServer
 		this.modules = modules;
 	}
 	
-	private void start(String[] args) throws Exception
+	private void launch(String[] args)
 	{
 		for (int index = 0; index < args.length; index++)
 		{
@@ -31,8 +36,15 @@ public class AppServer
 		}
 		
 		if (projectPath == null)
-			throw new Exception("Project path wasn't passed as argument");
+			throw new RuntimeException("Path to project wasn't set");
 		
-		new AppServerLauncher().launch(HOST, PORT, projectPath, modules);
+		try
+		{
+			new AppServerLauncher().launch(HOST, PORT, projectPath, modules);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
--- ne-server/modules/ru.nodthar1107.ne.appserver/src/main/java/ru/nodthar1107/ne/appserver/AppServerLauncher.java
+++ ne-server/modules/ru.nodthar1107.ne.appserver/src/main/java/ru/nodthar1107/ne/appserver/AppServerLauncher.java
@@ -2,10 +2,7 @@ package ru.nodthar1107.ne.appserver;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -23,8 +20,8 @@ import com.google.inject.Injector;
 public class AppServerLauncher {
 	private ServerSocket server;
 	private Socket socket;
-	private InputStream in;
-	private OutputStream out;
+	private BufferedInputStream in;
+	private BufferedOutputStream out;
 
 	public void launch(String host, int port, String projectPath, AbstractModule... modules) throws NoSuchAlgorithmException, IOException {
 		Injector injector = Guice.createInjector(modules);
@@ -36,8 +33,8 @@ public class AppServerLauncher {
 			server = new ServerSocket(port, 1, InetAddress.getByName("localhost"));
 			socket = server.accept();
 			
-			in = socket.getInputStream();
-			out = socket.getOutputStream();
+			in = new BufferedInputStream(socket.getInputStream());
+			out = new BufferedOutputStream(socket.getOutputStream());
 			Scanner s = new Scanner(in, "UTF-8");
 			
 			System.out.println(String.format("Connesction is set. Remote address: %s",
@@ -55,6 +52,7 @@ public class AppServerLauncher {
 				    + Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-1").digest((match.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes("UTF-8")))
 				    + "\r\n\r\n").getBytes("UTF-8");
 				  out.write(response, 0, response.length);
+				  out.flush();
 			}
 			
 			listen();
@@ -75,25 +73,76 @@ public class AppServerLauncher {
 	{
 		while (true)
 		{
-			byte[] encoded = new byte[1024];
-			int count = in.read(encoded);
-			System.out.println(count);
-			String message = decode(encoded, count);
+			String message = readMessage();
 			System.out.println(String.format("Message: %s", message));
+			byte[] res = ("Aboba biba").getBytes("UTF-8");
+			out.write(res, 0, res.length);
+			out.flush();
 		}
 	}
 	
-	private String decode(byte[] encoded, int count)
+	private String readMessage()
 	{
-		byte[] decoded = new byte[count];
-		byte[] key = new byte[] { (byte) 167, (byte) 225, (byte) 225, (byte) 210 };
-		for (int index = 0; index < count; index++)
+		try
+		{	
+			// Read first byte
+			in.read();
+			
+			int sizeDescriptor = in.read();
+			int messageSize = 0;
+			int offset = 0;
+			if (sizeDescriptor - 128 <= 125) {
+				messageSize = sizeDescriptor - 128;
+				offset = 2;
+			}
+			else if (sizeDescriptor - 128 == 126)
+			{
+				byte[] size = new byte[2];
+				in.read(size);
+				
+				messageSize = (int) (((size[0] & 0xFF) << 8) | (size[1] & 0xFF));
+				offset = 4;
+			}
+			else if (sizeDescriptor - 128 == 127)
+			{
+				byte[] size = new byte[4];
+				in.read(size);
+				
+				messageSize = (int) ((size[0] & 0xFF) << 24 | (size[1] & 0xFF) << 16 | (size[2] & 0xFF) << 8 | (size[3] & 0xFF));
+				offset = 6;
+			}
+			
+			byte[] key = new byte[4];
+			in.read(key);
+			offset += 4;
+			
+			byte[] buffer = new byte[1024];
+			StringBuilder builder = new StringBuilder();
+			while (messageSize > 0)
+			{
+				int readed = in.read(buffer);
+				messageSize -= readed;
+				
+				builder.append(decode(buffer, key, readed));
+			}
+			
+			return builder.toString();
+		}
+		catch (Exception e)
 		{
-			System.out.print(String.format("%d ", encoded[index]));
-			decoded[index] = (byte) (encoded[index] ^ key[index & 0x3]);
+			e.printStackTrace();
 		}
 		
-		System.out.println();
+		return null;
+	}
+	
+	private String decode(byte[] buffer, byte[] key, int length)
+	{
+		byte[] decoded = new byte[length];
+		for (int index = 0; index < length; index++)
+		{
+			decoded[index] = (byte)(buffer[index] ^ key[index & 0x3]);
+		}
 		
 		return new String(decoded);
 	}
